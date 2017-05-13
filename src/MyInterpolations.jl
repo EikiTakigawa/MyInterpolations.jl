e27776349bec25f1051d3e70a9660f337288254a
EikiTakigawa <E.Shine@outlook.jp>
2017-05-13 19:17:24 +0900

    ソースコードを追加

---
+++ src/MyInterpolations.jl
@@ -0,0 +1,27 @@
+
+module MyInterpolations
+
+immutable MyLinInterp
+    grid
+    vals
+end
+
+function (f::MyLinInterp)(x)
+    if x < f.grid[1]
+            y = ((f.vals[2]-f.vals[1])/(f.grid[2]-f.grid[1]))*(x-f.grid[1])+f.vals[1]
+            return y
+    end
+    if x >= f.grid[end]
+         y = ((f.vals[end]-f.vals[end-1])/(f.grid[end]-f.grid[end-1]))*(x-f.grid[end-1])+f.vals[end-1]
+        return y
+    end
+    if f.grid[1] <= x < f.grid[end]
+        i = searchsortedlast(f.grid, x)
+        y = ((f.vals[i+1]-f.vals[i])/(f.grid[i+1]-f.grid[i]))*(x-f.grid[i])+f.vals[i]
+        return y
+    end
+end
+
+end
+
+
