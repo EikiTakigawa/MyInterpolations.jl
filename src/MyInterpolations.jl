5be8ebd17043b903bb40e3d1349ee0f39b8f3c2d
EikiTakigawa <E.Shine@outlook.jp>
2017-10-10 11:31:12 +0900

    全体的に改善

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -4,26 +4,20 @@ module MyInterpolations
 export MyLinInterp
 
 struct MyLinInterp
-    grid::Array
-    vals::Array
+    grid::Array{Float64}
+    vals::Array{Float64}
 end
 
-function (f::MyLinInterp)(x::Number)
+function (f::MyLinInterp)(x::Float64)
     if x < f.grid[1]
             y = ((f.vals[2]-f.vals[1])/(f.grid[2]-f.grid[1]))*(x-f.grid[1])+f.vals[1]
-            return y
-    end
-    if x >= f.grid[end]
+    elseif x >= f.grid[end]
          y = ((f.vals[end]-f.vals[end-1])/(f.grid[end]-f.grid[end-1]))*(x-f.grid[end-1])+f.vals[end-1]
-        return y
-    end
-    if f.grid[1] <= x < f.grid[end]
+    else
         i = searchsortedlast(f.grid, x)
         y = ((f.vals[i+1]-f.vals[i])/(f.grid[i+1]-f.grid[i]))*(x-f.grid[i])+f.vals[i]
-        return y
     end
+    return y
 end
 
 end
-
-
